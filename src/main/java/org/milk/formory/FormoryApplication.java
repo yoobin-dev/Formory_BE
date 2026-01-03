7cd4f61127fd7d23d9c149df5d51efc5f3ebd557
yoobinlee <yoobinlee@yoobinleeui-MacBookPro.local>
2026-01-03 17:33:21 +0900

    initial project

---
+++ src/main/java/org/milk/formory/FormoryApplication.java
@@ -0,0 +1,13 @@
+package org.milk.formory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormoryApplication.class, args);
+	}
+
+}
